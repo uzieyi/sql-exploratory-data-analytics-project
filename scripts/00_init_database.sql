5671df7499ebc7bb4c305dafc2666729bd4c1e87
Christian Uzieyi <36804345+uzieyi@users.noreply.github.com>
2025-10-24 07:09:54 +0100

    Update file paths for bulk inserts in SQL script

--- scripts/00_init_database.sql
+++ scripts/00_init_database.sql
@@ -81,7 +81,7 @@ TRUNCATE TABLE gold.dim_customers;
 GO
 
 BULK INSERT gold.dim_customers
-FROM 'C:\sql\sql-data-analytics-project\datasets\csv-files\gold.dim_customers.csv'
+FROM 'C:\Users\HP\Documents\dataset\sql-data-warehouse-project\gold.dim_customers.csv'
 WITH (
 	FIRSTROW = 2,
 	FIELDTERMINATOR = ',',
@@ -93,7 +93,7 @@ TRUNCATE TABLE gold.dim_products;
 GO
 
 BULK INSERT gold.dim_products
-FROM 'C:\sql\sql-data-analytics-project\datasets\csv-files\gold.dim_products.csv'
+FROM 'C:\Users\HP\Documents\dataset\sql-data-warehouse-project\gold.dim_products.csv'
 WITH (
 	FIRSTROW = 2,
 	FIELDTERMINATOR = ',',
@@ -105,7 +105,7 @@ TRUNCATE TABLE gold.fact_sales;
 GO
 
 BULK INSERT gold.fact_sales
-FROM 'C:\sql\sql-data-analytics-project\datasets\csv-files\gold.fact_sales.csv'
+FROM 'C:\Users\HP\Documents\dataset\sql-data-warehouse-project\gold.fact_sales.csv'
 WITH (
 	FIRSTROW = 2,
 	FIELDTERMINATOR = ',',
